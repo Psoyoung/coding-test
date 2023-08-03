e03e076255390b15e55b03362fa705187f41ff91
Psoyoung <95350250+Psoyoung@users.noreply.github.com>
2023-08-03 23:28:36 +0900

    [unrated] Title: 카운트 다운, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int start, int end) {
+        int[] answer = new int[start-(end-1)];
+
+        int index = 0;
+        
+        for(int i = start; end <= i; i--){
+           answer[index] = i;
+           index++;
+        }        
+        
+        return answer;
+    }
+}
