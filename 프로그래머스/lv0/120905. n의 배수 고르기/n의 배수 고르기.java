b3b01e487742c4385df7aaab1df98feba0d88691
Psoyoung <95350250+Psoyoung@users.noreply.github.com>
2023-08-26 23:45:44 +0900

    [level 0] Title: n의 배수 고르기, Time: 1.96 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int[] numlist) {
+        numlist = Arrays.stream(numlist)
+                        .filter(i -> i % n == 0)
+                        .toArray();
+        return numlist;
+    }
+}
