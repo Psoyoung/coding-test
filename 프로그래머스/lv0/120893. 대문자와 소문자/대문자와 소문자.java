8cb08dfdadd518866040bc1ece1d5a21b728e807
Psoyoung <95350250+Psoyoung@users.noreply.github.com>
2023-08-30 23:50:02 +0900

    [level 0] Title: 대문자와 소문자, Time: 4.06 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.java
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -1,6 +1,7 @@
 class Solution {
     public String solution(String my_string) {
         String answer = "";
+        
         for(char c : my_string.toCharArray()){
             if(Character.isUpperCase(c)){
                 answer += String.valueOf(c).toLowerCase();
