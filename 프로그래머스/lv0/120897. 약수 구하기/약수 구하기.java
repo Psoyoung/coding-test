5dd3d335f32ddaf92d9b4c3e0e4b7f761d7752cb
Psoyoung <95350250+Psoyoung@users.noreply.github.com>
2023-09-04 23:56:31 +0900

    [level 0] Title: 약수 구하기, Time: 1.89 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int[] solution(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i = 1; i <= n ; i++){
+            if(n % i == 0) list.add(i);
+        }
+        
+        int[] answer = list.stream()
+                            .mapToInt(i -> i)
+                            .toArray();
+        return answer;
+    }
+}
