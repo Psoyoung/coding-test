016de1c90be33c3701632a53b318a6b9ed2b63df
Psoyoung <95350250+Psoyoung@users.noreply.github.com>
2023-09-06 00:05:40 +0900

    [unrated] Title: 날짜 비교하기, Time: 9.64 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int[] date1, int[] date2) {
+        String dt1 = date1[0] + "" + date1[1] + "" + date1[2];
+        String dt2 = date2[0] + "" + date2[1] + "" + date2[2];
+        
+        return Integer.parseInt(dt1) < Integer.parseInt(dt2) ? 1 : 0;
+    }
+}
