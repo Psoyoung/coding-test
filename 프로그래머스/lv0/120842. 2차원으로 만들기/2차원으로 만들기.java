6842dd4e4ce468b1fea07b1d2b0c481a5acb3108
Psoyoung <95350250+Psoyoung@users.noreply.github.com>
2023-09-20 00:29:02 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.04 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int[][] solution(int[] num_list, int n) {
+        
+        int[][] answer = new int[num_list.length/n][n];
+        
+        for(int i = 0, j = 0; i < num_list.length/n; i++, j+=n){
+           answer[i] = Arrays.copyOfRange(num_list, j, j+n);
+        }
+        
+        return answer;
+    }
+}
