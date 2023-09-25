826ff06137534f3fa4e11dac99833a7ffb96bcb1
Psoyoung <95350250+Psoyoung@users.noreply.github.com>
2023-09-26 01:16:16 +0900

    [level 0] Title: 7의 개수, Time: 0.02 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int num : array){
+            
+            while(num > 0){
+                if(num%10 == 7) answer++;
+                num /= 10;
+            }
+        }
+        
+        return answer;
+    }
+}
