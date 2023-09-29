8cbdeaabd623bef18e5e6c79714aefb1b6409c5d
Psoyoung <95350250+Psoyoung@users.noreply.github.com>
2023-09-30 00:17:08 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.32 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int a, int b) {
+        
+        Double db = (double) a / b ;
+        String dbStr = String.valueOf(db);
+
+        return dbStr.length() >= 16 ? 2 : 1;
+    }    
+}
