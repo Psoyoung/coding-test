da6e42e63236dc3e48ab149aa2766f8f9fbfa1be
Psoyoung <95350250+Psoyoung@users.noreply.github.com>
2023-10-04 23:14:20 +0900

    [level 0] Title: 옹알이 (1), Time: 1.09 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        for(String str : babbling){
+            str = str.replaceAll("aya","-");
+            str = str.replaceAll("ye","-");
+            str = str.replaceAll("woo","-");
+            str = str.replaceAll("ma","-");
+            str = str.replaceAll("-","");
+            
+            if(str.equals("")) answer++;
+        }
+        return answer;
+    }
+}
