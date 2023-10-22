b1b0fc56dd5c4dbaf56da45e547cdc1eb7b56330
Psoyoung <95350250+Psoyoung@users.noreply.github.com>
2023-10-22 14:10:55 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.02 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        
+        answer[0] = gcd(n,m);
+        answer[1] = lcm(n,m);
+        
+        return answer;
+    }
+    
+    //최대 공약수
+    public static int gcd(int a,int b){
+        if(b == 0){
+            return a;
+        }else{
+            return gcd(b, a % b);
+        }
+    }
+    
+    //최대 공배수
+    public static int lcm(int a,int b){
+        int g = gcd(a,b);
+        return g * (a/g) * (b/g);
+    }
+}
